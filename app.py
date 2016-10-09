4ab3ec43e132d50f0ae44b4c186e8895dc4a34a6
blocktrron <blocktrron@users.noreply.github.com>
2016-10-09 20:57:31 +0200

    do not cache retrieved data

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ import requests
 
 app = Flask(__name__, static_url_path='/static')
 vehicle_data = {"last_updated": 0, "successful": False, "vehicles": []}
-failed_attempts = 0
-update_interval = 15
 
 
 @app.route('/')
@@ -18,29 +16,20 @@ def show_page():
 
 @app.route("/vehicledata")
 def load_vehicledata():
-    global vehicle_data, failed_attempts
-    if vehicle_data["last_updated"] + update_interval <= time.time():
-        r = requests.get('https://routing.geomobile.de/v4/vehiclelivedata?bundleIdentifier=de.ivanto.heagmobilo')
-        if r.status_code is 200:
-            root_obj = json.loads(r.text)
-            if "vehicles" in root_obj:
-                for vehicle in root_obj["vehicles"]:
-                    del vehicle["encodedPath"]
-                    vehicle["latitude"] = round(vehicle["latitude"], 6)
-                    vehicle["longitude"] = round(vehicle["longitude"], 6)
-                vehicle_data = root_obj
-                vehicle_data["last_updated"] = time.time()
-                vehicle_data["successful"] = True
-            return json.dumps(vehicle_data, ensure_ascii=False), 200, {
-                'Content-Type': 'application/json; charset=utf-8'}
-        else:
-            vehicle_data["successful"] = False
+    global vehicle_data
+    r = requests.get('https://routing.geomobile.de/v4/vehiclelivedata?bundleIdentifier=de.ivanto.heagmobilo')
+    if r.status_code is 200:
+        root_obj = json.loads(r.text)
+        if "vehicles" in root_obj:
+            for vehicle in root_obj["vehicles"]:
+                del vehicle["encodedPath"]
+                vehicle["latitude"] = round(vehicle["latitude"], 6)
+                vehicle["longitude"] = round(vehicle["longitude"], 6)
+            vehicle_data = root_obj
             vehicle_data["last_updated"] = time.time()
-            failed_attempts = + 1
-            if (failed_attempts > 8):
-                vehicle_data["vehicles"] = {}
-            return json.dumps(vehicle_data, ensure_ascii=False), 200, {
-                'Content-Type': 'application/json; charset=utf-8'}
+            vehicle_data["successful"] = True
+        return json.dumps(vehicle_data, ensure_ascii=False), 200, {
+            'Content-Type': 'application/json; charset=utf-8'}
     else:
         return json.dumps(vehicle_data, ensure_ascii=False), 200, {
             'Content-Type': 'application/json; charset=utf-8'}
